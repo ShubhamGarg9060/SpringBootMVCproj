091d3c7cdfaff5d29f05768186e061b9fe7a3fc1
ShubhamGarg9060 <s.garg9060@gmail.com>
2023-01-08 01:00:41 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootmvcproj/SpringBootMvCprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvcproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvCprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMvCprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmvcproj/SpringBootMvCprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvcproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvCprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
